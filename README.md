2b3f301c791a7130033f16e21785f254f3524931
bily-101 <billy1775@gmail.com>
2020-10-06 14:54:58 +0000

    Main

--- README.md
+++ README.md
@@ -1,4 +1,113 @@
-# VirtualChatAssistant
-Based off the tutorial:
+# How to make a Virtual Chat Assistant In python
 
-How to make a Virtual Chat Assistant
+ Install packages
+
+
+* sudo apt-get install portaudio19-dev python-pyaudio
+* pip install pyaudio
+* pip install SpeechRecognition 
+* pip install gTTS
+* pip install wikipedia
+
+
+Import Librarys
+
+```python
+import speech_recognition as sr
+import os
+from gtts import gTTS
+import datetime
+import warnings
+import calendar
+import random
+import wikipedia
+```
+
+Put in function recording
+
+```
+def recording():    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:  
+       print('Say something!')
+       audio = r.listen(source)
+        data = ''
+    try:
+        data = r.recognize_google(audio)
+        print('You said: ' + data)
+    except sr.UnknownValueError:
+        print('Google Speech Recognition could not understand')
+    except sr.RequestError as e:
+        print('Request error from Google Speech Recognition')
+     
+    return data
+
+
+
+```
+
+Function for response
+
+```
+def response(text):
+    print(text)
+    myobj = gTTS(text=text, lang='en', slow=False)
+    
+    myobj.save('assistant_response.mp3')
+
+    os.system('start assistant_response.mp3')
+```
+
+Function to awaken the computer or test for a response
+```
+def wakeWord(text):
+    WAKE_WORDS = ['hey computer', 'okay computer'] 
+    text = text.lower()  # Convert the text to all lower case words
+    for phrase in WAKE_WORDS:
+        if phrase in text:
+            return True
+    return False
+```
+
+Function for greeting
+
+```
+def greeting(text):
+    # Greeting Inputs
+    GREETING_INPUTS = ['yo', 'hey', 'hola', 'greetings', 'wassup', 'hello']
+    GREETING_RESPONSES = ['Hi', 'How can I help you', 'hello', 'hey there']
+    for word in text.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES) + '.'
+    return ''
+```
+
+Function for knowing your name
+
+```
+def getName(text):
+ wordList = text.split()# Split the text into a list of words     
+ for i in range(0, len(wordList)):
+   if i + 3 <= len(wordList) - 1 and wordList[i].lower() == 'who' and             wordList[i + 1].lower() == 'is':
+    return wordList[i + 2] + ' ' + wordList[i + 3]
+```
+
+
+Putting all functions together for the main
+
+```
+while True:
+    text = record()
+    response = '' 
+    if (wakeWord(text) == True):
+        if ('who is' in text):
+            person = getName(text)
+            wiki = wikipedia.summary(person, sentences=2)            
+            response = response + ' ' + wiki
+       
+       response(response)
+      ```
+
+
+Then Run using 
+python file
--- index.py
+++ index.py
@@ -7,17 +7,13 @@ import calendar
 import random
 import wikipedia
 
-warnings.filterwarnings('ignore')
 
-def recordAudio():    
-    # Record the audio
+def recording():    
     r = sr.Recognizer()
     with sr.Microphone() as source:  
-       print('Say something!')
-       audio = r.listen(source)
-    
-    # Speech recognition using Google's Speech Recognition
-    data = ''
+        print('Say something!')
+        audio = r.listen(source)
+        data = ''
     try:
         data = r.recognize_google(audio)
         print('You said: ' + data)
@@ -25,103 +21,42 @@ def recordAudio():
         print('Google Speech Recognition could not understand')
     except sr.RequestError as e:
         print('Request error from Google Speech Recognition')
-     
+
     return data
 
-def assistantResponse(text):
+def response(text):
     print(text)
-    # Convert the text to speech
     myobj = gTTS(text=text, lang='en', slow=False)
     
-    # Save the converted audio to a file    
     myobj.save('assistant_response.mp3')
-    # Play the converted file
+
     os.system('start assistant_response.mp3')
-    
-def wakeWord(text):
-    WAKE_WORDS = ['hey computer', 'okay computer'] 
-    text = text.lower()  # Convert the text to all lower case words
-  # Check to see if the users command/text contains a wake word    
-    for phrase in WAKE_WORDS:
-        if phrase in text:
-            return True
-  # If the wake word was not found return false
-    return False
 
-def getDate():
-    
-    now = datetime.datetime.now()
-    my_date = datetime.datetime.today()
-    weekday = calendar.day_name[my_date.weekday()]# e.g. Monday
-    monthNum = now.month
-    dayNum = now.day
-    month_names = ['January', 'February', 'March', 'April', 'May',
-       'June', 'July', 'August', 'September', 'October', 'November',   
-       'December']
-    ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th', 
-                      '7th', '8th', '9th', '10th', '11th', '12th',                      
-                      '13th', '14th', '15th', '16th', '17th', 
-                      '18th', '19th', '20th', '21st', '22nd', 
-                      '23rd', '24th', '25th', '26th', '27th', 
-                      '28th', '29th', '30th', '31st']
-   
-    return 'Today is ' + weekday + ' ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1] + '.'
-
-# Function to return a random greeting response
 def greeting(text):
     # Greeting Inputs
-    GREETING_INPUTS = ['hi', 'hey', 'hola', 'greetings', 'wassup', 'hello']
-     # Greeting Response back to the user
-    GREETING_RESPONSES = ['howdy', 'whats good', 'hello', 'hey there']
-     # If the users input is a greeting, then return random response
+    GREETING_INPUTS = ['yo', 'hey', 'hola', 'greetings', 'wassup', 'hello']
+    GREETING_RESPONSES = ['Hi', 'How can I help you', 'hello', 'hey there']
     for word in text.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES) + '.'
-    # If no greeting was detected then return an empty string
     return ''
 
-def getPerson(text):
+   
+def getName(text):
     wordList = text.split()# Split the text into a list of words     
     for i in range(0, len(wordList)):
-        if i + 3 <= len(wordList) - 1 and wordList[i].lower() == 'who' and wordList[i + 1].lower() == 'is':
+        if i + 3 <= len(wordList) - 1 and wordList[i].lower() == 'who' and             wordList[i + 1].lower() == 'is':
             return wordList[i + 2] + ' ' + wordList[i + 3]
-    
 
+    
 while True:
-    # Record the audio
-    text = recordAudio()
-    response = '' #Empty response string
-     
-    # Checking for the wake word/phrase
+    text = recording()
+    response = '' 
     if (wakeWord(text) == True):
-         # Check for greetings by the user
-        response = response + greeting(text)
-         # Check to see if the user said date
-        if ('date' in text):
-            get_date = getDate()
-            response = response + ' ' + get_date
-         # Check to see if the user said time
-        if('time' in text):
-            now = datetime.datetime.now()
-            meridiem = ''
-            if now.hour >= 12:
-                meridiem = 'p.m' #Post Meridiem (PM)
-                hour = now.hour - 12
-            else:
-                meridiem = 'a.m'#Ante Meridiem (AM)
-                hour = now.hour
-           # Convert minute into a proper string
-            if now.minute < 10:
-                minute = '0'+str(now.minute)
-            else:
-                minute = str(now.minute)
-                response = response + ' '+ 'It is '+ str(hour)+ ':'+minute+' '+meridiem+' .'
-                
-        # Check to see if the user said 'who is'
         if ('who is' in text):
-            person = getPerson(text)
+            person = getName(text)
             wiki = wikipedia.summary(person, sentences=2)            
             response = response + ' ' + wiki
        
-        assistantResponse(response)
-    
+    response(response)
+      
